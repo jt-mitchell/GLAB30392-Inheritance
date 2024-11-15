74f66646273b86ac4c0ff9b16913f81558bd8a4a
Jeff Mitchell <jeffreytm@gmail.com>
2024-11-15 16:28:48 -0500

    first commit

---
+++ src/Hospital.java
@@ -0,0 +1,14 @@
+
+public class Hospital {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Surgeon s = new Surgeon();
+	       s.Doctor_Details();
+	       s.Surgeon_Details();
+
+
+	}
+
+}
---
+++ src/Surgeon.java
@@ -0,0 +1,9 @@
+public class Surgeon extends Doctor {
+		 void Surgeon_Details() {
+		       System.out.println("Surgeon  Detail...");
+		       System.out.println(Department = "Cardio");
+		  
+		 }
+		 
+		 
+}
